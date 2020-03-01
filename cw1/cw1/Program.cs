c86f0dff7e30dd01834b4875325cf950a417f510
Mateusz Jacoszek <s16532@pjwstk.edu.pl>
2020-03-01 19:08:45 +0100

    initial commit

---
+++ cw1/cw1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Gas the jews !!!");
+
+            var emails = await GetEmails(args[0]);
+
+            foreach(var a in args)
+            {
+                Console.WriteLine(a);
+            }
+
+            foreach ( var email in emails)
+            {
+                Console.WriteLine(email);
+            }
+        }
+
+        static async Task<IList<string>> GetEmails(string url)
+        {
+            var httpclient = new HttpClient();
+            var listOfEmails = new List<string>();
+
+            var response = await httpclient.GetAsync(url);
+
+
+            Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*",
+            RegexOptions.IgnoreCase);
+            //find items that matches with our pattern
+            MatchCollection emailMatches = emailRegex.Matches(response.Content.ReadAsStringAsync().Result);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Match emailMatch in emailMatches)
+            {
+                listOfEmails.Add(emailMatch.ToString());
+            }
+
+            return listOfEmails;
+        }
+    }
+}
